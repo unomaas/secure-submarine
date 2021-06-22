d0d3e2808fd5f3adaaa40e54fd13a9d4ba85ee2f
Ryan Maas <rdmjobs@live.com>
2021-06-22 15:12:52 -0500

    Base mode complete.

--- server/modules/authentication-middleware.js
+++ server/modules/authentication-middleware.js
@@ -10,4 +10,16 @@ const rejectUnauthenticated = (req, res, next) => {
   }
 };
 
-module.exports = { rejectUnauthenticated };
+const rejectUnauthorized = (req, res, next) => {
+  // check if logged in
+  if (req.body.secrecy_level <= req.user.clearance_level) {
+    // They were authenticated! User may do the next thing
+    // Note! They may not be Authorized to do all things
+    next();
+  } else {
+    // failure best handled on the server. do redirect here.
+    res.sendStatus(403);
+  }
+};
+
+module.exports = { rejectUnauthenticated, rejectUnauthorized };
--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const { rejectUnauthenticated, rejectUnauthorized } = require('../modules/authentication-middleware');
 
 
 router.get('/', rejectUnauthenticated, (req, res) => {
@@ -9,7 +9,10 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   console.log('req.user:', req.user);
 
   pool
-    .query(`SELECT * FROM "secret";`)
+    .query(`
+      SELECT * FROM "secret"
+      WHERE "secrecy_level" <= $1;
+    `, [req.user.clearance_level])
     .then((results) => res.send(results.rows))
     .catch((error) => {
       console.log('Error making SELECT for secrets:', error);
